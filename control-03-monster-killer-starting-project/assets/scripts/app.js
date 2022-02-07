761f66031b5c1598529fe11490f5e0ebeee135af
Monica Delgado <mnca.delgado@gmail.com>
2022-02-07 09:51:03 -1100

    added prompt and isNaN functions

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -4,7 +4,14 @@ const STRONG_ATTACK_VALUE = 17;
 const MOSNTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximun life for you and the mosnter.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
