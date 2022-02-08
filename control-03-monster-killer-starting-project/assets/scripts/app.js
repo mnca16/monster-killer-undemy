0b01c55b86c08784af2f862a047a149c2af8ff04
Monica Delgado <mnca.delgado@gmail.com>
2022-02-08 11:45:15 -1100

    added switch statements to the writeToLog function

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -34,16 +34,24 @@ function writeToLog(ev, val, monsterHealt, playerHealth) {
         finalMonsterHealth: monsterHealt,
         finalPlayerHealth: playerHealth
     };
-    if(ev === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry.target = 'MONSTER';
-    } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry.target = 'MONSTER';
-    } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry.target = 'PLAYER';
-    } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-        logEntry.target = 'PLAYER'
-    } else if (ev === LOG_EVENT_GAME_OVER) {
-        logEntry;
+    switch (ev) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry;
+            break;
+        default:
+            logEntry = {};
     }
 
     battleLog.push(logEntry);
